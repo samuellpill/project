f2e53fc8bbe6588c6ec95752e0a4d57392cc4adf
samuellpill <82765663+samuellpill@users.noreply.github.com>
2021-07-11 20:49:32 +0530

    Create project.cpp

---
+++ project.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"enter your age ";
+	cin>>age;
+	if(age>=18)
+	{
+		cout<<"\n YOU CAN HAVE HARD DRINKS \t smirnoff \t budwiser\t mc downlaods  ";
+	}
+	else
+	{
+		cout<<"\n you can drink brezzer\n water too";
+	}
+	return 0;
+	
+}
